a2cecf49a38262fb2421e655ce72de179789611a
Jason Jakary <91029212+jasonjakary@users.noreply.github.com>
2024-10-25 20:24:07 -0500

    Update requirements.sh

--- requirements.sh
+++ requirements.sh
@@ -2,4 +2,5 @@
 pip3 install proxmoxer --break-system-packages
 pip3 install "PySimpleGUI<5.0.0" --break-system-packages
 pip3 install requests --break-system-packages
+touch /etc/vdiclient/vdiclient.ini
 cp vdiclient.ini.example /etc/vdiclient/vdiclient.ini
